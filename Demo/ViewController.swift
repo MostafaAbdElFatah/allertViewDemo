49951e9ae0652fac41674f46259277cfeceed36d
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2019-07-23 10:30:11 +0200

    allert view libraries

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AMSmoothAlert
+import FCAlertView
+import CFAlertViewController
 
 class ViewController: UIViewController {
 
+    var alert:AMSmoothAlertView! = nil
+    var alertView:FCAlertView!
+    var alertController:CFAlertViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ///MARK:- AMSoothAlertView
+        //alert = AMSmoothAlertView(dropAlertWithTitle: "Failed", andText: "Error Message you do mistake", andCancelButton: true, for: .success, andColor: .green)
+        ///MARK:- FCAlertView
+        alertView = FCAlertView(frame: self.view.frame)
+        alertView.titleColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        alertView.titleFont = UIFont(name: "HelveticaNeue-Bold", size: 20.0)
+        //alertView.fullCircleCustomImage = true
+        //alertView.animateAlertOutToTop = true
+        alertView.animateAlertOutToBottom = true
+        alertView.animateAlertInFromTop = true
+        //alertView.customImageScale = 1.5
+        //alertView.darkTheme = true
+        
+        ///MARK:- CFAlertViewController
+//        alertController = CFAlertViewController(title: "Failed",message: "Error Message you do mistake",textAlignment: .center,preferredStyle: .alert,didDismissAlertHandler: nil)
+//        let imageView = UIImageView(image: UIImage(named: "error"))
+//        imageView.contentMode = .center
+//        alertController.headerView = imageView
+//        let actionBtn = CFAlertAction(title: "Ok", style: .Cancel, alignment: CFAlertAction.CFAlertActionAlignment.center, backgroundColor: .white, textColor: .red, handler: nil)
+//        alertController.addAction(actionBtn)
     }
 
-
+    @IBAction func showAlert_btnClicked(_ sender: UIButton) {
+        //alert.show()
+        alertView.showAlert(withTitle: "Failed", withSubtitle: "Error Message you do mistake", withCustomImage: UIImage(named: "error") , withDoneButtonTitle: "Ok", andButtons:nil)
+//        self.present(self.alertController, animated: true, completion: nil)
+        
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'Demo' do
+  use_frameworks!
+
+  # Pods for Demo
+  pod 'CFAlertViewController'
+  pod 'FCAlertView'
+  pod 'AMSmoothAlert', '~> 1.0'
+
+end
